1859e2bbe78dfc0b2cfd7d47d89e44fb79a4918c
Omkar Maurya <51255950+Omkarmaurya@users.noreply.github.com>
2026-02-21 22:45:31 +0530

    Update prepare_playwright_env.sh

--- prepare_playwright_env.sh
+++ prepare_playwright_env.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Install playwright and its dependencies
-npx -y playwright@latest install --with-deps webkit
+npx -y playwright@latest install --with-deps firefox
 
 # install node modules
 npm install
