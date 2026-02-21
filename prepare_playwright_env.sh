7e8bfc23a975627624f1378fce2303f1f67f6a11
Omkar Maurya <51255950+Omkarmaurya@users.noreply.github.com>
2026-02-21 23:09:01 +0530

    Update prepare_playwright_env.sh

--- prepare_playwright_env.sh
+++ prepare_playwright_env.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-export FONTCONFIG_PATH=/tmp
-sudo chmod -R a+w /tmp
+PLAYWRIGHT_BROWSERS_PATH=$HOME/pw-browsers
+
+sudo chmod -R a+w $HOME/pw-browsers
 
 # Install playwright and its dependencies
 npx -y playwright@latest install --with-deps firefox
